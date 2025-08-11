07fdd091e0e2bf866231efaed9e0abddd5e773cf
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-08-11 20:54:05 +0300

    updated the name and version and created a separate readme for pypi before publishing

--- pff_omar_arabi/cli/cli.py
+++ pff_omar_arabi/cli/cli.py
@@ -7,7 +7,7 @@ class Cli:
 
 
     def setup(self) -> Namespace:
-        parser = argparse.ArgumentParser(prog="fs", description="a fuzzy search for files and diretories")
+        parser = argparse.ArgumentParser(prog="pff", description="a fuzzy finder for files and diretories")
         parser.add_argument("-v", "--version", action="version", version=f"%(prog)s v{self.version}")
         parser.add_argument("dir", type=str, help="the directory to search in")
         parser.add_argument("pattern", type=str, help="the pattern to search for in the file name")
