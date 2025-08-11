24ae701cc02e45a76a7bece425d0b56a195e9c0e
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-08-11 18:21:10 +0300

    made the tests pass and added the filter option

--- fs_omar_arabi/__main__.py
+++ fs_omar_arabi/__main__.py
@@ -1,11 +1,15 @@
 from argparse import Namespace
 
-from cli.cli import Cli
+from .cli.cli import Cli
+from .fuzzy_search.fuzzy_search import fuzzy_search
 from . import __version__
 
 def main():
     cli: Cli = Cli(version=__version__)
     args: Namespace = cli.setup()
+    matches = fuzzy_search(args.dir, args.pattern, args.filter)
+    for match in matches:
+        print(match)
 
 
 if __name__ == "__main__":
