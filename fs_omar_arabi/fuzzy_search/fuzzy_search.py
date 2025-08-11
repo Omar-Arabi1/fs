aa6c7a807d1ae7c3247774edad69fadad7be54c1
Omar-Arabi1 <omarbuisness7@gmail.com>
2025-08-11 18:24:57 +0300

    added the failed test for the hidden option

--- fs_omar_arabi/fuzzy_search/fuzzy_search.py
+++ fs_omar_arabi/fuzzy_search/fuzzy_search.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-def fuzzy_search(dir: str, pattern: str, filter_by: str) -> list[str]:
+def fuzzy_search(dir: str, pattern: str, filter_by: str, hidden: bool) -> list[str]:
     if not os.path.exists(dir) or os.path.isfile(dir):
         print(f"{dir} is invalid", file=sys.stderr)
         sys.exit(1)
@@ -12,7 +12,7 @@ def fuzzy_search(dir: str, pattern: str, filter_by: str) -> list[str]:
     if filter_by == "files":
         entries = filter(lambda entry: os.path.isfile(entry), entries)
     elif filter_by == "dirs":
-        entries = list(filter(lambda entry: os.path.isdir(entry), entries))
+        entries = filter(lambda entry: os.path.isdir(entry), entries)
     elif filter_by != "":
         print(f"invalid option for filter", file=sys.stderr)
         sys.exit(1)
--- tests/test_fuzzy_search.py
+++ tests/test_fuzzy_search.py
@@ -1,16 +1,22 @@
 from fs_omar_arabi.fuzzy_search.fuzzy_search import fuzzy_search
 
 def test_fuzzy_search():
-    got = fuzzy_search(dir='.', pattern="pypr", filter_by="")
+    got = fuzzy_search(dir='.', pattern="pypr", filter_by="", hidden=False)
     want = ["pyproject.toml"]
     assert got == want
 
 
 def test_fuzzy_search_with_filter():
-    got = fuzzy_search(dir='.', pattern=".gi", filter_by="files")
+    got = fuzzy_search(dir='.', pattern=".gi", filter_by="files", hidden=True)
     want = [".gitignore"]
     assert got == want
 
-    got = fuzzy_search(dir=".", pattern=".gi", filter_by="dirs")
+    got = fuzzy_search(dir=".", pattern=".gi", filter_by="dirs", hidden=True)
     want = [".git"]
+    assert got == want
+
+
+def test_fuzzy_search_with_hidden():
+    got = fuzzy_search(dir='.', pattern=".gi", filter_by="", hidden=False)
+    want = []
     assert got == want
